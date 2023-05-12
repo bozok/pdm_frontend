abe92d802e744448a54f839334d515408b87a63f
Fatih <bozok17@gmail.com>
2023-05-12 23:09:02 +0300

    another deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pdm-frontend/",
+  base: "/pdm_frontend/",
   plugins: [react()],
   build: {
     manifest: true,
